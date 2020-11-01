a5340cbf956b0a79ae2e4214b371deff5c2246d5
Joost de Niet <17049644@student.hhs.nl>
2020-11-01 13:19:16 +0100

    Controller input setup

---
+++ Assets/Scripts/VR Rig/continuesMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class continuesMovement : MonoBehaviour
+{
+	/// <summary>
+	/// Variables
+	/// </summary>
+	[Header("Settings")]
+	public AxisHandler2D inputAxisController;
+
+	/// <summary>
+	/// Onenable and Disable controller input event
+	/// </summary>
+	public void OnEnable()
+	{
+		inputAxisController.OnValueChange += SetAxisMovement;
+	}
+
+	public void OnDisable()
+	{
+		inputAxisController.OnValueChange -= SetAxisMovement;
+	}
+
+	private void Update()
+	{
+		
+	}
+
+	private void SetAxisMovement(XRController controller, Vector2 axis)
+	{
+		Debug.Log(axis);
+	}
+}
